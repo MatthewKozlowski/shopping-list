4e0fc7b7e41ba3e394e7a85e8ef0164fde9cd72f
Matthew Kozlowski <maekozlowski@gmail.com>
2019-03-03 08:33:43 -0700

    added js to check, delete, & add to shopping list

---
+++ index.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        let listItem = $('#shopping-list-entry').val();
+        $('.shopping-list').append(`<li><span class="shopping-item">${listItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+    });
+    
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(){
+        $(this).parent().prev().toggleClass('shopping-item__checked');
+    });
+
+
+    $('.shopping-list').on('click', '.shopping-item-delete', function(){
+        $(this).parent().parent().remove();
+    });
+
+})
